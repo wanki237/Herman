537bd95763704fa6a03e7550415102befc78e7d1
unknown <wankiherman0@gmail.com>
2025-03-11 10:01:45 +0100

    welcome to bambili

--- class.py
+++ class.py
@@ -5,4 +5,6 @@ class Human:
         self.height=height
         self.gender=gender
 c1= Human("john",50,1.95,"M")
+c2= Human("herman",50,1.67,"M")
 print(f"My name is {c1.name} and i am {c1.age} old")
+print(f"Mey name is {c2.name} and i am {c2.age} old")
